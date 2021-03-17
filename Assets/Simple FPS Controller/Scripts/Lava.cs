a2c26ae3d6ae7a8a2c18d4532facee8e04c75290
Nate Nicholson <nchlsnnate@gmail.com>
2021-03-17 12:31:30 -0500

    Adding FPS controller

---
+++ Assets/Simple FPS Controller/Scripts/Lava.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lava : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            SceneTransition.mainSceneTransition.ChangeScene(-1);
+        }
+    }
+}
